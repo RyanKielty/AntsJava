fb12ad92c6333f16d7a3c79887fa37aa19a06688
ryankielty <rpkielty@gmail.com>
2017-01-19 10:38:25 -0600

    initial

---
+++ src/com/theironyard/Ant.java
@@ -0,0 +1,14 @@
+package com.theironyard;
+
+/**
+ * Created by ryankielty on 1/19/17.
+ */
+public class Ant {
+    double x;
+    double y;
+
+    public Ant(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+}
---
+++ src/com/theironyard/Main.java
@@ -0,0 +1,76 @@
+package com.theironyard;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.stream.Collectors;
+
+public class Main extends Application {
+    static final int WIDTH = 800;
+    static final int HEIGHT = 600;
+
+    static final int ANT_COUNT = 100;
+
+    ArrayList<Ant> ants;
+
+    static  ArrayList<Ant> createAnts() {
+        ArrayList<Ant> ants = new ArrayList<>();
+        for (int i = 0; i < ANT_COUNT; i++) {
+            Random randomObject = new Random();
+            ants.add(new Ant(randomObject.nextInt(WIDTH), randomObject.nextInt(HEIGHT)));
+        }
+        return ants;
+    }
+
+    static  double randomStep() {
+        return Math.random() * 2-1;
+    }
+
+    Ant moveAnt(Ant ant) {
+        ant.x += randomStep();
+        ant.y += randomStep();
+        return ant;
+    }
+
+    void moveAnts() {
+        ants = ants.stream()
+                .map(this::moveAnt)
+                .collect(Collectors.toCollection(ArrayList<Ant>::new));
+    }
+
+    void drawAnts(GraphicsContext context) {
+        context.clearRect(0, 0, WIDTH, HEIGHT);
+        for (Ant ant : ants) {
+            context.setFill(Color.BLACK);
+            context.fillOval(ant.x, ant.y, 5, 5);
+        }
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("main.fxml"));
+        Scene scene= new Scene(root, WIDTH, HEIGHT);
+
+        primaryStage.setTitle("Ants");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+
+        Canvas canvasObject = (Canvas) scene.lookup(("#canvas"));
+        GraphicsContext context = canvasObject.getGraphicsContext2D();
+
+        ants = createAnts();
+        drawAnts(context);
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
